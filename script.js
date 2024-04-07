c2eb5c1bd230a198110b4e0e9a67bd9b9e51af5d
Tussis88 <andreab88@hotmail.it>
2024-04-07 15:50:18 +0200

    riformulata la funzione di creazione griglia
    creata funzione changeWidth assegnata al bottone

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-const width = 16;
-const height = 16;
+const gridWidth = 16;
 
 const container = document.querySelector("#container");
+const button = document.querySelector("#gridButton");
+button.textContent = `grid: ${gridWidth}`;
 
-for (let i = 0; i < width; i++) {
-  for (let j = 0; j < height; j++) {
+const createGrid = (width) => {
+  for (let i = 0; i < width * width; i++) {
     const pixel = document.createElement("div");
     pixel.className = "pixel";
     pixel.addEventListener("mouseover", (e) => {
-      console.log(`w: ${i}, h: ${j}`);
       e.target.style.backgroundColor = "#eb6f92";
     });
     container.appendChild(pixel);
   }
-}
+};
+
+const changeWidth = () => {
+  let newWidth = prompt("Set the new grid resolution:");
+  while (isNaN(newWidth) || newWidth > 64 || newWidth < 0) {
+    newWidth = prompt(
+      `${newWidth} is not a valid value. You need to insert a number beetween 1 and 64`,
+    );
+  }
+  console.log(newWidth);
+  const oldGrid = document.querySelectorAll("#pixel");
+};
+
+createGrid(gridWidth);
